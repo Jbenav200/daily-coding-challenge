1fda0e9388b6a432ede8cc4221419878b0662425
Jonathan Yaniv Ben Avraham <jbenav200@caledonian.ac.uk>
2021-03-23 22:55:15 +0000

    updated formatted output

--- Day 1/challenge1.py
+++ Day 1/challenge1.py
@@ -2,11 +2,12 @@ class CheckSums:
     potential_solutions = {}
     array = [10, 14, 2, 3]
 
-    def checksums(self, k):
-        for num in self.array:
+    def checksums(self, array, k):
+        for num in array:
             if num in self.potential_solutions:
                 return f'The array contains two numbers which add up to {k}'
-            if k - num in self.array:
+            if k - num in array:
                 self.potential_solutions[k - num] = num
-                print(f'{k-num} => {self.potential_solutions[k-num]} \n')
+                print(
+                    f'array pairings: {k - num} => {self.potential_solutions[k-num]} \n')
         return f'The array does not contain two numbers which add up to {k}'
--- Day 1/main.py
+++ Day 1/main.py
@@ -2,9 +2,13 @@ from challenge1 import CheckSums
 
 
 def main():
+    array = [[10, 14, 2, 3], [10, 14, 2, 4]]
     k = 17
     cs = CheckSums()
-    print(cs.checksums(k))
+    print(f'\n\nchecking arrays: {array[0]} and {array[1]} \n')
+    for x in array:
+        print(f'for array: {x}')
+        print(f'{cs.checksums(x, k)} \n')
 
 
 if __name__ == "__main__":
