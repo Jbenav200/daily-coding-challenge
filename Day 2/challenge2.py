e68e5baf1c1eeff4c5b853010cc2f2e8457367a8
Jonathan Yaniv Ben Avraham <jbenav200@caledonian.ac.uk>
2021-03-24 20:10:20 +0000

    completed daily challenge two

--- Day 2/challenge2.py
+++ Day 2/challenge2.py
@@ -1,4 +1,21 @@
 class ChallengeTwo():
 
-    def some_method(self):
-        pass
+    # Define the product_except_self method
+    def product_except_self(self, arr):
+        # Assign an array the same length as arr
+        right_mult = [0] * len(arr)
+        # assigning the last index of arr to right_mult's last index
+        right_mult[-1] = arr[-1]
+
+        for i in range(1, len(arr)):
+            right_mult[len(arr) - i - 1] = right_mult[len(arr) -
+                                                      i] * arr[len(arr) - i - 1]
+        result = [0] * len(arr)
+        pref = 1
+        index = 0
+        while index < len(result) - 1:
+            result[index] = pref * right_mult[index+1]
+            pref *= arr[index]
+            index += 1
+        result[-1] = pref
+        return result
--- Day 2/main.py
+++ Day 2/main.py
@@ -1,11 +1,33 @@
 from challenge2 import ChallengeTwo
+import random
 
 
 def main():
-    # do something
+    # Given Test Data
+    arr1 = [1, 2, 3, 4, 5]
+
+    # Random test data
+    arr2 = []
+
+    # For each value in arr1, assign a new random value between 1 and 14
+    # and add it to the end of arr2
+    for x in arr1:
+        x = random.randint(1, 14)
+        arr2.append(x)
+
+    # Instantiate the ChallengeTwo Class
     challenge_two = ChallengeTwo()
-    return f'{challenge_two.some_method}'
+
+    # Compile both arrays in a single array to couple up the results
+    arr3 = [arr1, arr2]
+    # For each array in arr3, return the result of product_except_self method
+    # in the ChallengeTwo class
+    for x in arr3:
+        res = challenge_two.product_except_self(x)
+        # Print the result to the terminal.
+        print(f'\nthe result of passing {x} is: {res}\n')
 
 
+# This method is what causes the main method to run
 if __name__ == "__main__":
     main()
